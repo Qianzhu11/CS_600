22f18cae157ada923a215b159162a4f9e184cc4a
ZZQ <2858583565@qq.com>
2017-11-29 12:34:12 +0800

    update

--- src/SearchEngine.java
+++ src/SearchEngine.java
@@ -10,6 +10,7 @@ public class SearchEngine {
 	public static final String startPage = "https://www.stevens.edu/news";
 	Trie t = new Trie();
 	Loader l = new Loader();
+	Map<String, String> pages = new HashMap<String, String>();
 	List<String> stopWordList = Arrays.asList(new String[]{"a", "above", "about", "after", "again", "against", 
 			"all", "am", "an", "only", "or", "other", "ought", "our", "ours", "on", "once","of", "off",
 			"and", "any", "are", "as", "at", "be", "because", "been", "being", "below", "bwtween",
@@ -40,6 +41,13 @@ public class SearchEngine {
 	public void insertToTrie(String page) {
 		Document doc = l.loadPage(page);
 		Elements e = doc.select("p");
+		String s1 = "";
+		
+		for (int i = 0; i < e.size(); i++) {
+			s1 += e.get(i).toString().toLowerCase();
+		}
+		pages.put(page, s1);
+		
 		for (int i = 0; i < e.size(); i++) {
 			String s = e.get(i).toString().toLowerCase();
 			String[] li = s.replaceAll("[\\pP\\p{Punct}]", "").split("[.,\\s]+");
@@ -62,17 +70,76 @@ public class SearchEngine {
 		}
 	}
 	
-	public HashSet<String> getResult(String[] s) {
-		HashSet<String> res = new HashSet<String>();
+	public List<String> getResult(String[] s) {
+		List<String> res = new ArrayList<String>();
+		List<String> web = new ArrayList<String>();
+		
 		for (int i = 0; i < s.length; i++) {
-			ArrayList<String> s1 = invertedFile.get(s[i]);
-			for (String s2 : s1) res.add(s2);
+			if (!invertedFile.containsKey(s[i])) {
+				return null;
+			} else res.add(s[i]);
 		}
-		return res;
+		if (res.size() == 1) return invertedFile.get(res.get(0));
+		else {
+			web = invertedFile.get(res.get(0));
+			for (int i = 1; i < res.size(); i++) {
+				web = intersection(web, invertedFile.get(res.get(i)));
+			}
+			return web;
+		}
+	}
+	
+	public <T> List<T> intersection(List<T> list1, List<T> list2) {
+        List<T> list = new ArrayList<T>();
+        
+        for (T t : list1) {
+            if(list2.contains(t)) {
+                list.add(t);
+            }
+        }
+        return list;
+    }
+	
+	public List<String> sort(List<String> res, String[] words) {
+		List<String> newRes = new ArrayList<String>(res.size());
+		List<Integer> freq = new ArrayList<Integer>(res.size());
+		for (int i = 0; i < res.size(); i++) {
+			int count = 0;
+			String s1 = pages.get(res.get(i));
+			for (int j = 0; j < words.length; j++) {
+				count += countMatches(s1, words[j]);
+			}
+			freq.add(count);
+		}
+		while (!freq.isEmpty()) {
+			int max = freq.get(0);
+			int delete = 0;
+			for (int i = 0; i < freq.size(); i++) {
+				if (freq.get(i) > max) {
+					delete = i;
+					max = freq.get(i);
+				}
+			}
+			newRes.add(res.get(delete));
+			res.remove(delete);
+			freq.remove(delete);
+		}
+		return newRes;
+	}
+	
+	public static int countMatches(String str, String sub) {
+	      int count = 0;
+	      int idx = 0;
+	      while ((idx = str.indexOf(sub, idx)) != -1) {
+	          count++;
+	          idx += sub.length();
+	      }
+	      return count;
 	}
 	
 	public static void main(String[] args) {
 		SearchEngine se = new SearchEngine();
+		
 		se.insertToTrie(startPage);
 		Elements links = se.l.loadPage(startPage).select("a[href]");
 		for (int i = 0; i < 10; i++) {
@@ -81,24 +148,34 @@ public class SearchEngine {
 		}
 		List<String> li = new ArrayList<String>();
 		Scanner s = new Scanner(System.in);
+		
 		System.out.println("Please type the words you want to search for:");
-		System.out.println("type quit() to exit");
+		System.out.println("type quit() to end typing.");
+		
 		while (true) {
 			if (s.hasNextInt()) {
 				System.out.println("You must provide valid words.");
 				return;
 			}
 			String line = s.nextLine();
+			
 			if (line.equals("quit()")) break; 
 			li.add(line);
 		}
 		String[] s1 = new String[li.size()];
 		s1 = li.toArray(s1);
-		HashSet<String> res = se.getResult(s1);
-		System.out.println("Here are the results:");
-		for (String s2 : res) {
-			System.out.println(s2);
-		}
+		boolean containStopWords = false;
+		for (int i = 0; i < s1.length; i++) if (se.stopWords.contains(s1[i])) containStopWords = true;
+		if (containStopWords == true) System.out.println("Your are searching the stop words.");
+		else {
+			List<String> res = se.getResult(s1);
+			if (res == null) System.out.println("Sorry, no such word or words combination in these websites.");
+			else {
+				List<String> newRes = se.sort(res, s1);
+				System.out.println("Here are the results(sorted by words frequence):");
+				for (String s2 : newRes) System.out.println(s2);
+			}
+		}	
 	}
 }
 
--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,40 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Test {
-
-	public static void main(String[] args) {
-		 Scanner s = new Scanner(System.in); 
-         System.out.println("ÇëÊäÈë×Ö·û´®£º"); 
-         while (true) { 
-                 String line = s.nextLine(); 
-                 if (line.equals("exit")) break; 
-                 System.out.println(line); 
-         } 
+	
+	public static int countMatches(String str, String sub) {
+	      int count = 0;
+	      int idx = 0;
+	      while ((idx = str.indexOf(sub, idx)) != -1) {
+	          count++;
+	          idx += sub.length();
+	      }
+	      return count;
+	}
+	  
+	
+	public static void main(String[] args) throws Exception {
+		Test t = new Test();
+		List<Integer> l = new ArrayList<Integer>();
+		l.add(0, 4);
+		l.add(1, 2);
+		l.add(2, 4);
+		l.add(3, 1);
+		System.out.println(l.set(0, 1));
+		System.out.println(l.set(0, 2));
+		while (!l.isEmpty()) {
+			int max = l.get(0);
+			int delete = 0;
+			for (int i = 0; i < l.size(); i++) {
+				if (l.get(i) > max) {
+					delete = i;
+					max = l.get(i);
+				}
+			}
+			System.out.println(l.get(delete));
+			l.remove(delete);
+		}
+		
+		
 	}
 }
